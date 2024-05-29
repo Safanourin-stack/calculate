56a7f24548976e572e81ae11a7488bec55a99593
Safa-stack <safanourinpk@gmail.com>
2024-05-29 14:35:15 +0530

    first

---
+++ calc.js
@@ -0,0 +1,19 @@
+function display(n)
+{
+  result.value+=n
+}
+
+function fin(){
+
+  result.value=eval(result.value)
+}
+
+function cle()
+{
+  result.value=" "
+}
+
+function bac()
+{
+  result.value=result.value.slice(0,-1)
+}
